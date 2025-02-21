ffee6001da9e08c9b75944b1d05b5e031f780411
sumukhbendre123 <s_vbendre@ee.iitr.ac.in>
2025-02-22 00:11:57 +0530

    hashing

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,8 +1,29 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcrypt.js";
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
     try {
         //hash pasword
-        const hashedPassword = await bcrypt.hash(password, 10);
+        if (password.length < 6) {
+            return res.status(400).json({ message: " Password must be at least 6 characters" });
+        }
+        const user = await User.findOne({ email })
+
+        if (user) return res.status(400).json({ message: "Email already exisits" });
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword,
+        })
+        if (newUser) {
+
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
     } catch (error) {
 
     }
