8eee9040b0b34d8bbeb9dc19ba803042249cdd43
sumukhbendre123 <s_vbendre@ee.iitr.ac.in>
2025-03-29 00:48:27 +0530

    Few bugs are reaining to fix rest all things are added

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
-
 const userSchema = new mongoose.Schema(
     {
         email: {
             type: String,
             required: true,
             unique: true,
-
         },
         fullName: {
             type: String,
